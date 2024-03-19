4fd4040d63b2694f9c01a4f3691d8d1b80fe1f2e
matt <matt@MATTHEWALLI1D7A>
2024-03-19 19:22:39 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10269378_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
